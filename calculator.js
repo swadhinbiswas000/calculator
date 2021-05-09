f6f776f7d3586658479bba0f914f46783ef8a681
swadhinbiswas000 <swadhincpa@gmail.com>
2021-05-09 10:25:05 +0600

    Calculator Initial commit

---
+++ calculator.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.sendFile((__dirname)+ "/index.html");
+});
+
+app.post("/", function(req, res){
+    var number1 =Number(req.body.num1);
+    var number2 =Number(req.body.num2);
+
+    var result = number1 + number2;
+
+    res.send("The result of the calculation is " + result);
+})
+
+
+app.listen(3000, function(){
+    console.log("app started on 3000");
+});
